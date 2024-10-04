bc77a2c4b7f6c58f7cf057955021f498cc84fee6
courageousstrawberry <elech2a@gmail.com>
2024-10-03 20:56:17 -0700

    add some code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    string name;
+    string trait;
+    cout << "Hello World! Please enter the name of your world: ";
+    cin >> name;
 
+    cout << "Wow! " << name << " seems like it would be a cool place to live." << endl;
+    cout << "Input one of " << name << "'s traits: ";
+    cin >> trait;
+
+    cout << "Name of world: " << name << endl;
+    cout << "-------------" << endl;
+    cout << "Trait: " << trait << endl;
+    
     return 0;
 }
